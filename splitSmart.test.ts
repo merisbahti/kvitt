3b8cbd8b9692fa8fa4c7ed66e07c623d67f5e201
meris <meris.bahti@gmail.com>
2025-04-19 07:01:59 +0200

    fix algo

---
+++ splitSmart.test.ts
@@ -0,0 +1,92 @@
+import * as R from 'remeda'
+import {test, expect} from 'bun:test'
+
+const splitPayments = <Keys extends string>(contributions: Record<Keys, number>): {
+    averageCost: number,
+    diffs: Record<Keys, number>,
+    debtsFrom: Record<Keys, Partial<Record<Keys, number>>>
+} => {
+
+    const averageCost = R.pipe(
+        contributions,
+        R.values,
+        R.sum(),
+        (n) => R.divide(n, R.length(R.keys(contributions)))
+    )
+
+    const diffs = R.pipe(
+        contributions,
+        R.mapValues((value) => averageCost - value)
+    ) as unknown as Record<Keys, number>
+
+    type DebtValue = Partial<Record<Keys, number>>
+    const debtsFrom = Object.fromEntries(Object.keys(contributions).map(x => [x, {}] as const)) as unknown as { [key in Keys]: DebtValue }
+    const getBalanceFor = (name: Keys) => {
+        const diff = diffs[name] // negative means they owe money
+
+
+        const debtsForName = R.sum(R.values(Object.values(debtsFrom[name]) as DebtValue) as number[])
+
+        return diff + debtsForName // this is the balance for the name, we want it to be 0
+    }
+
+
+    for (const [payer, totalToPay] of R.entries.strict(diffs)) {
+        for (const [borrower, initial] of R.entries.strict(debtsFrom)) {
+            const payerBalance = getBalanceFor(payer)
+            console.log(`payer: ${payer}, borrower: ${borrower} payerBalance: ${payerBalance}`)
+            if (payerBalance >= 0) {
+                console.log('skipping payer', payer)
+                continue
+            }
+
+            const borrowerBalance = getBalanceFor(borrower)
+
+            if (borrowerBalance <= 0) {
+                console.log('skipping borrower', borrower)
+                continue
+            }
+
+            const amountToPay = Math.min(-payerBalance, borrowerBalance)
+            console.log(`payer: ${payer} borrower: ${borrower} amountToPay: ${amountToPay}`)
+            debtsFrom[payer] = {...debtsFrom[payer], [borrower]: amountToPay}
+        }
+
+    }
+
+
+    return {averageCost, diffs, debtsFrom}
+}
+
+
+test('splits wisely', () => {
+    const simpleExample = {
+        'meris': 300,
+        'henrietta': 150
+    }
+    const output = splitPayments(simpleExample)
+    expect(output.averageCost).toEqual(225)
+    expect(output.diffs.henrietta).toEqual(75)
+    expect(output.diffs.meris).toEqual(-75)
+    expect(output.debtsFrom.meris).toEqual({henrietta: 75})
+    expect(output.debtsFrom.henrietta).toEqual({})
+
+})
+
+test('three way example', () => {
+    const simpleExample = {
+        'meris': 300,
+        'henrietta': 150,
+        'sara': 50
+    }
+    const output = splitPayments(simpleExample)
+    expect(output.averageCost).toEqual(500 / 3)
+    expect(output.diffs.henrietta).toEqual(500 / 3 - 150)
+    expect(output.diffs.meris).toEqual(500 / 3 - 300)
+    expect(output.diffs.sara).toEqual(500 / 3 - 50)
+    expect(output.debtsFrom).toEqual({
+        'meris': {henrietta: (500 / 3 - 150), sara: (500 / 3 - 50)},
+        'henrietta': {},
+        'sara': {}
+    })
+})
