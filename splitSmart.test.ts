c5d555d585e50ef186011886d8fbe47bd966010f
meris <meris.bahti@gmail.com>
2025-04-20 10:54:04 +0200

    working example

--- splitSmart.test.ts
+++ splitSmart.test.ts
@@ -35,6 +35,41 @@ const splitPayments = (people: Array<string>, initialPayments: Array<{
 
     const paymentsForBalance: Database = []
 
+    let currentBalance = {...initialBalance}
+    let attemptsLeft = people.length * people.length
+    console.log('initialBalance', initialBalance)
+    while (Object.values(currentBalance).some(x => x !== 0) && attemptsLeft > 0) {
+        attemptsLeft--
+
+        // find min and max
+
+        const extemes = Object.entries(currentBalance).reduce((acc, tuple) => {
+            const [name, value] = tuple as [string, number]
+            if (value === 0) {
+                return acc
+            }
+            const max = ((!acc.max) || value > acc.max.value) ? {person: name, value} : acc.max
+            const min = ((!acc.min) || value < acc.min.value) ? {person: name, value} : acc.min
+            return {min, max}
+        }, {min: null, max: null} as {
+            min: { person: string, value: number } | null,
+            max: { person: string, value: number } | null
+        })
+
+        if (!extemes.min || !extemes.max || extemes.max.person === extemes.min.person) {
+            break
+        }
+
+
+        paymentsForBalance.push({
+            from: extemes.max.person,
+            to: extemes.min.person,
+            payment: Math.abs(Math.max(extemes.min.value, extemes.max.value))
+        })
+
+        currentBalance = getBalance([...database, ...paymentsForBalance])
+    }
+
 
     return {initialBalance, paymentsForBalance}
 }
@@ -96,7 +131,19 @@ test('three way example', () => {
         from: 'henrietta',
         amount: 2
     }])
-    expect(output.initialBalance).toEqual({})
+    expect(output.initialBalance).toEqual({
+        henrietta: 1.0000000000000004,
+        meris: -4,
+        sara: 3,
+    })
+    expect(output.paymentsForBalance).toEqual(
+        [
+            {
+                from: 'sara',
+                to: 'meris',
+                payment: 3
+            }, {from: 'henrietta', to: 'meris', payment: 1.0000000000000004}])
+
 })
 
 test.skip('real world test', () => {
