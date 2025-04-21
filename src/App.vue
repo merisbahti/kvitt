5f023229bf51f94be8c76eaf55f7413a961f4e56
meris <meris.bahti@gmail.com>
2025-04-21 08:43:29 +0200

    main thing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,95 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { getKvitt } from './getKvitt'
+type State = {
+  party: string[]
+  initialPayments: Array<{
+    from: string
+    amount: number | Record<string, number>
+  }>
+}
+const localState = ref<State>({
+  party: [],
+  initialPayments: [],
+})
 </script>
 
-<template>hi</template>
+<template>
+  <div class="main-flow">
+    <h1>Kvitt</h1>
+    <div>
+      <h2>Party</h2>
+      <div class="party">
+        <input
+          v-for="(member, index) in localState.party"
+          :key="index"
+          v-model="localState.party[index]"
+        />
+        <button @click="localState.party.push('')">Add member</button>
+      </div>
+    </div>
+    <div>
+      <h2>Payments</h2>
+      <div class="payments">
+        <div
+          class="payment-row"
+          v-for="(payment, index) in localState.initialPayments"
+          :key="index"
+        >
+          <div class="payment-input">
+            <label for="payment-from">From</label>
+            <select name="payment-from" id="payment-from" v-model="payment.from">
+              <option v-for="(member, index) in localState.party" :key="index" :value="member">
+                {{ member }}
+              </option>
+            </select>
+          </div>
+          <div class="payment-input">
+            <label for="payment-amount">Amount</label>
+            <input type="text" name="payment-amount" id="payment-amount" v-model="payment.amount" />
+          </div>
+        </div>
+        <button
+          class="add-payment-button"
+          @click="localState.initialPayments.push({ from: '', amount: 0 })"
+        >
+          Add payment
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.party {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+}
+.payments {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.payment-row {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+}
+
+.payment-input {
+  display: flex;
+  flex-direction: column;
+}
+
+.add-payment-button {
+  width: fit-content;
+}
+
+.main-flow {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
